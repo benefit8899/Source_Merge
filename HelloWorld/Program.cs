55dc0ba1823575e4c510981dadb539b4b0333725
Van Loi <vanloi.infotech@gmail.com>
2016-06-11 19:16:52 +0700

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("sdfsdf");
+            Console.Write("Hello World!!");
             Console.Read();
         }
     }
