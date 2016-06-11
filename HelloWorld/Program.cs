96dc0e8d05b6cee0c1715bc33831a680d06d03c0
key <benefit8899@gmail.com>
2016-06-11 19:04:30 +0700

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Xin chao!!");
+            Console.Write("Xin chao !!");
             Console.Read();
         }
     }
