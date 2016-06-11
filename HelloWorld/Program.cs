4b3b099459af1a1b0a2da18afc2506a8e05a7fc6
benefit8899 <vanloi.infotech@gmail.com>
2016-06-11 17:43:30 +0700

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("");
+            Console.Write("sdfsdf");
             Console.Read();
         }
     }
